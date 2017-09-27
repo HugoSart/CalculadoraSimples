7d907a20b2cbf0f29ee6002a6199c14cb6693d95
Vinicius <vinicamarggo@hotmail.com>
2017-09-27 17:23:05 -0300

    Initial commit

---
+++ app/src/main/java/android/dominando/calculadorasimples/MainActivity.java
@@ -0,0 +1,27 @@
+package android.dominando.calculadorasimples;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+
+    EditText etNum1;
+    EditText etNum2;
+    Button btSoma;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        etNum1 = (EditText)findViewById(R.id.etNum1);
+        etNum2 = (EditText)findViewById(R.id.etNum2);
+        btSoma = (Button)findViewById(R.id.btSoma);
+
+    }
+
+
+
+}
